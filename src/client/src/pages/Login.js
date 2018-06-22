365ef1acfe1704f50655ff126ed3bf4e4abb1428
Ernesto Fuentes <Ernesto.Fuentes@macys.com>
2018-06-22 13:08:27 -0400

    Adding login page styling

--- src/client/src/pages/Login.js
+++ src/client/src/pages/Login.js
@@ -22,30 +22,42 @@ class Login extends Component {
 	render() {
 		return (
 			<section id="login-page">
+				<h1>Sign in</h1>
 				<form className="login-form" onSubmit={this.onSubmitHandler}>
-					<h1>Please Login to handle your CV</h1>
-					<hr />
-					<TextInputField
-						labelText="Email :"
-						id="login-email"
-						name="email"
-						type="email"
-						value={this.state.email}
-						placeholder="email address..."
-						onChange={this.onChangeHandler}
-						required
-					/>
-					<TextInputField
-						labelText="Password :"
-						id="login-password"
-						name="password"
-						type="password"
-						value={this.state.password}
-						placeholder="password address..."
-						onChange={this.onChangeHandler}
-						required
-					/>
-					<button type="submit">Log In</button>
+					<div className="form-fields">
+						<TextInputField
+							labelText="Email :"
+							id="login-email"
+							name="email"
+							type="email"
+							value={this.state.email}
+							placeholder="email address..."
+							onChange={this.onChangeHandler}
+							required
+						/>
+						<TextInputField
+							labelText="Password :"
+							id="login-password"
+							name="password"
+							type="password"
+							value={this.state.password}
+							placeholder="password address..."
+							onChange={this.onChangeHandler}
+							required
+						/>
+						<div className="options-wrapper">
+							<div className="remember-me">
+								<div className="form-field">
+									<input type="checkbox" id="remb" />
+									<label htmlFor="remb">Remember me</label>
+								</div>
+								<a href="/forgot">Forgot password?</a>
+							</div>
+							<button className="btn btn-success" type="submit">
+								Log In
+							</button>
+						</div>
+					</div>
 				</form>
 			</section>
 		)
